c0ae409902ab490c586baf4fdab3782cdf5c82f0
Rolfi <leandro.sobko@gmail.com>
2023-07-11 11:59:22 -0300

    front end iniciado

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavbarM from './components/NavbarM.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <NavbarM/>
+      </div>
+    </>
+  )
+}
+
+export default App
