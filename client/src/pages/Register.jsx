cfb2137f219022c8d15843b16f50d750ae03ff7f
dreschpedro <99pedrodresch@gmail.com>
2023-07-13 11:17:28 -0300

    pages -> register

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+export class Register extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Register
