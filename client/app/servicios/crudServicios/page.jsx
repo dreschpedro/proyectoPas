31976b1740c3b2e39170a38f71c6e9b361c6994d
Rolfi <leandro.sobko@gmail.com>
2023-07-20 11:59:39 -0300

    front-servicios

---
+++ client/app/servicios/crudServicios/page.jsx
@@ -0,0 +1,162 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Table, Button, Modal, Form } from 'react-bootstrap';
+
+const ServicesCrud = () => {
+  const [services, setServices] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
+    id: '',
+    name: '',
+    description: '',
+    // Otros campos de servicio aquí
+  });
+
+  // Datos de ejemplo para simular cuatro servicios
+  const servicesData = [
+    {
+      id: 1,
+      name: 'Servicio 1',
+      description: 'Descripción del Servicio 1',
+    },
+    {
+      id: 2,
+      name: 'Servicio 2',
+      description: 'Descripción del Servicio 2',
+    },
+    {
+      id: 3,
+      name: 'Servicio 3',
+      description: 'Descripción del Servicio 3',
+    },
+    {
+      id: 4,
+      name: 'Servicio 4',
+      description: 'Descripción del Servicio 4',
+    },
+  ];
+
+  useEffect(() => {
+    // Aquí usaríamos fetchServices para obtener los datos reales desde el backend
+    // Como solo tenemos datos de ejemplo, establecemos directamente los datos ficticios
+    setServices(servicesData);
+  }, []);
+
+  const handleShowModal = (service) => {
+    if (service) {
+      setFormData(service);
+    } else {
+      setFormData({
+        id: '',
+        name: '',
+        description: '',
+      });
+    }
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Lógica para enviar los datos del formulario al backend para crear o actualizar el servicio
+      if (formData.id) {
+        // Lógica para actualizar servicio
+      } else {
+        // Lógica para crear servicio
+      }
+      handleCloseModal();
+      // Como estamos usando datos ficticios, simplemente actualizamos la lista de servicios
+      setServices(servicesData);
+    } catch (error) {
+      console.error('Error al guardar el servicio:', error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      // Lógica para eliminar el servicio utilizando el backend
+      // Actualiza la lista de servicios después de eliminar
+      // Como estamos usando datos ficticios, simplemente actualizamos la lista de servicios
+      setServices(servicesData.filter((service) => service.id !== id));
+    } catch (error) {
+      console.error('Error al eliminar el servicio:', error);
+    }
+  };
+
+  return (
+    <Container>
+      <h1>CRUD de Servicios</h1>
+      <Button onClick={() => handleShowModal()}>Agregar Servicio</Button>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {services.map((service) => (
+            <tr key={service.id} style={{ marginBottom: '10px' }}>
+              <td>{service.id}</td>
+              <td>{service.name}</td>
+              <td>{service.description}</td>
+              <td>
+                <Button onClick={() => handleShowModal(service)}>Editar</Button>
+                <Button onClick={() => handleDelete(service.id)}>Eliminar</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{formData.id ? 'Editar Servicio' : 'Agregar Servicio'}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formName">
+              <Form.Label>Nombre del Servicio</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Descripción del Servicio</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {/* Agrega aquí más campos para el formulario de servicio */}
+            <Button variant="primary" type="submit">
+              Guardar
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+};
+
+export default ServicesCrud;
--- client/app/servicios/page.jsx
+++ client/app/servicios/page.jsx
@@ -1,10 +1,212 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import { Form, Button, InputGroup, Modal } from 'react-bootstrap';
 
-function servicios() {
+const RegistroServiciosRealizados = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedService, setSelectedService] = useState('');
+  const [serviceInfo, setServiceInfo] = useState({
+    dni: '',
+    fechaNacimiento: '',
+    ocupacion: '',
+    domicilioReal: '',
+    hijos: '',
+    trabajo: '',
+    subsidio: '',
+    institucion: '', // Combobox para la institución que realiza el servicio
+  });
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar el registro del servicio realizado
+    console.log("Información del servicio realizado:", {
+      selectedService,
+      ...serviceInfo,
+    });
+    // Luego, puedes enviar los datos al backend para almacenarlos en la base de datos
+    // y realizar otras operaciones necesarias
+    handleCloseModal();
+  };
+
   return (
-      <h1>servicios</h1>
- 
-     )
+    <>
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>Registrar Servicio Realizado</h1>
+      <br />
+
+      <Form onSubmit={handleSubmit}>
+
+      <Form.Group controlId="formInstitucion">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Institución
+            </InputGroup.Text>
+            <Form.Control
+              as="select"
+              value={serviceInfo.institucion}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, institucion: e.target.value })}
+              required
+            >
+              <option value="">Seleccionar Institución</option>
+              <option value="Institución 1">Institución 1</option>
+              <option value="Institución 2">Institución 2</option>
+              {/* Agrega aquí más opciones de instituciones */}
+            </Form.Control>
+          </InputGroup>
+        </Form.Group>
+
+
+        <Form.Group controlId="formService">
+          <InputGroup className="mb-3 mt-1">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Servicio Realizado
+            </InputGroup.Text>
+            <Form.Control
+              as="select"
+              value={selectedService}
+              onChange={(e) => setSelectedService(e.target.value)}
+              required
+            >
+              <option value="">Seleccionar Servicio</option>
+              <option value="Servicio 1">Servicio 1</option>
+              <option value="Servicio 2">Servicio 2</option>
+              {/* Agrega aquí más opciones de servicios */}
+            </Form.Control>
+            <Button variant="primary" onClick={handleShowModal} style={{ marginLeft: '10px' }}>
+              Agregar Servicio
+            </Button>
+          </InputGroup>
+        </Form.Group>
+
+        <h2 style={{ textAlign: 'center' }}>Información Socioambiental</h2>
+
+        {/* Información Socioambiental */}
+        <Form.Group controlId="formDni">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              DNI
+            </InputGroup.Text>
+            <Form.Control
+              type="text"
+              value={serviceInfo.dni}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, dni: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formFechaNacimiento">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Fecha de Nacimiento
+            </InputGroup.Text>
+            <Form.Control
+              type="date"
+              value={serviceInfo.fechaNacimiento}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, fechaNacimiento: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formOcupacion">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Ocupación
+            </InputGroup.Text>
+            <Form.Control
+              type="text"
+              value={serviceInfo.ocupacion}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, ocupacion: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formDomicilioReal">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Domicilio Real
+            </InputGroup.Text>
+            <Form.Control
+              type="text"
+              value={serviceInfo.domicilioReal}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, domicilioReal: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formHijos">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Hijos
+            </InputGroup.Text>
+            <Form.Control
+              type="number"
+              value={serviceInfo.hijos}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, hijos: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formTrabajo">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Trabajo
+            </InputGroup.Text>
+            <Form.Control
+              type="text"
+              value={serviceInfo.trabajo}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, trabajo: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="formSubsidio">
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Subsidio
+            </InputGroup.Text>
+            <Form.Control
+              type="text"
+              value={serviceInfo.subsidio}
+              onChange={(e) => setServiceInfo({ ...serviceInfo, subsidio: e.target.value })}
+              required
+            />
+          </InputGroup>
+        </Form.Group>
+
+
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <Button variant="primary" type="submit" style={{ width: '200px', fontWeight: 'bold' }}>
+            Registrar Servicio
+          </Button>
+        </div>
+      </Form>
+
+      {/* Ventana Modal para Agregar Servicios */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Agregar Servicios</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            {/* Agrega aquí los campos para agregar servicios */}
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
-export default servicios
+export default RegistroServiciosRealizados;
