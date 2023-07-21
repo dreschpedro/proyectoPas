98685026d61e47f33e3d2aa43f0c4167c3916cdc
dreschpedro <99pedrodresch@gmail.com>
2023-07-21 08:40:54 -0300

    string de coneccion

--- server/config/db.js
+++ server/config/db.js
@@ -1,22 +1,15 @@
-import pkg from 'pg';
-const { Pool } = pkg;
-import { Sequelize } from 'sequelize';
+import Sequelize from 'sequelize';
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize('proyecto_pas', 'postgres', 'admin', {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 export default sequelize;
