f6710c23005fdd924918fe41a26a6409321d4b97
Rolfi <leandro.sobko@gmail.com>
2023-08-02 11:55:47 -0300

    fornt-dashboard

--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Nav, Button } from 'react-bootstrap';
 import Link from 'next/link';
+import Row from 'react-bootstrap';
+import Col from 'react-bootstrap';
+
+// const buttonStyle = {
+//   backgroundColor: '#22096F',
+//   marginTop: '15px',
+//   color: '#FFFFFF',
+//   widht: '20px',
+// }
 
 const Sidebar = () => {
   return (
-    <div className="sidebar bg-secondary mt-5 pt-5  fixed-start h-100">
-      <Nav className="justify-content-center flex-grow-1 pe-3 pt-5">
-                  <Link href={"/estadisticas"}>
-                    <Button variant="secondary">Estadísticas</Button>
+    <div className="sidebar mt-5 pt-5  fixed-start h-100 " style={{backgroundColor: '#101488'}}>
+      <Nav className='d-flex flex-column justify-content-center align-items-center' style={{backgroundColor: '#101488'}}>
+                  
+                  <Link href={"/estadisticas"} style={{marginTop: '15px'}} className='custom-link'>
+                    <div className='buttoncito' >Estadísticas</div>
                   </Link>
-                  <Link href={"/instituciones"}>
-                    <Button variant="secondary">Instituciones</Button>
+                  <div className='buttoncito custom-link'>
+                  <Link href={"/instituciones"} className='custom-link'>
+                    Instituciones
                   </Link>
-                  <Link href={"/servicios"}>
-                    <Button variant="secondary">Servicios</Button>
+                  </div>
+                  <div className='buttoncito'>
+                  <Link href={"/servicios"} className='custom-link'>
+                    Servicios
                   </Link>
-                  <Link href={"/usuarios"}>
-                    <Button variant="secondary">Usuarios</Button>
+                  </div>
+                  <Link href={"/usuarios"} className='custom-link'>
+                    <div className='buttoncito'>Usuarios</div>
                   </Link>
                 </Nav>
     </div>
