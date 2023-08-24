6704c5b03762a7d04449ef5c8b8ea0b1a3307bdf
Rolfi <leandro.sobko@gmail.com>
2023-08-24 12:02:02 -0300

    front 248

--- client/app/admin/usuarios/page.jsx
+++ client/app/admin/usuarios/page.jsx
@@ -74,7 +74,7 @@ function Usuarios() {
           />
 
           <div className="ml-auto"  style={{ display: 'flex', marginLeft: 'auto' }}>
-            <Link href="usuarios/usuarioCreate">
+            <Link href="/admin/usuarios/usuarioCreate">
               <button className="buttonRegistrar" style={{ marginLeft: '3rem' }}>
                 Crear Usuario
               </button>
